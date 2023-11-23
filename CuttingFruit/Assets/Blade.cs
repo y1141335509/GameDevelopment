6cc6e030e8f82b5f26e904bf4b0dd4c136032266
y1141335509 <y1141335509@gmail.com>
2023-11-22 21:25:26 -0800

    defined blade velocity

--- CuttingFruit/Assets/Blade.cs
+++ CuttingFruit/Assets/Blade.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Blade : MonoBehaviour
 {
+    public GameObject bladeTrailPrefab;     // define the blade trail prefab
+    public float minCuttingVelocity = .01f;        // min cutitng velocity
+
     private bool isCutting = false;
+    Vector2 previousPosition;
+
+    GameObject currentBladeTrail;
 
     Rigidbody2D rb;
     Camera cam;     // define the main camera
+    CircleCollider2D circleCollider;
 
     void Start()
     {
         cam = Camera.main;                  // get the main camera
         rb = GetComponent<Rigidbody2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
@@ -33,18 +41,36 @@ public class Blade : MonoBehaviour
         }
     }
 
+    void UpdateCut()
+    {
+        Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition);      // the current mouse position.
+        rb.position = newPosition;
+        float velocity = (newPosition - previousPosition).magnitude * Time.deltaTime;
+
+        if (velocity > minCuttingVelocity)
+        {
+            circleCollider.enabled = true;
+        }
+        else
+        {
+            circleCollider.enabled = false;
+        }
+        previousPosition = newPosition;
+    }
+
     void StartCutting()
     {
         isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        circleCollider.enabled = true;
     }
 
     void StopCutting()
     {
         isCutting = false;
+        currentBladeTrail.transform.SetParent(null);    // 
+        Destroy(currentBladeTrail, 2f);                  // destroy the trails after 2 seconds
+        circleCollider.enabled = false;
     }
 
-    void UpdateCut()
-    {
-        rb.position = cam.ScreenToWorldPoint(Input.mousePosition);      // the current mouse position.
-    }
 }
