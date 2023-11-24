001ee3464b9f715465c134bc51387d7fbdb1ec39
y1141335509 <y1141335509@gmail.com>
2023-11-24 10:01:56 -0800

    created a ball class

---
+++ FruitNinja/core/src/com/enhighfood/game/Ball.java
@@ -0,0 +1,35 @@
+package com.enhighfood.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Ball {
+    int x;
+    int y;
+    int size;
+    int xSpeed;
+    int ySpeed;
+
+    public Ball(int x, int y, int size, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public void update() {
+        x += xSpeed;
+        y += ySpeed;
+        if (x < 0 || x > Gdx.graphics.getWidth()) {
+            xSpeed = -xSpeed;
+        }
+        if (y < 0 || y > Gdx.graphics.getWidth()) {
+            ySpeed = -ySpeed;
+        }
+    }
+
+    public void draw(ShapeRenderer shape) {
+        shape.circle(x, y, size);
+    }
+}
