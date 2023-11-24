d601f052d89937aa7ab85bbe0ddd620288db07ea
y1141335509 <y1141335509@gmail.com>
2023-11-23 16:03:52 -0800

    somewhat able to spawn fruit and cut

--- CuttingFruit/Assets/Scripts/Fruit.cs
+++ CuttingFruit/Assets/Scripts/Fruit.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Fruit : MonoBehaviour
 {
     public GameObject fruitSlicedPrefab;
+    Rigidbody2D rb;
+    public float startForce = 15f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -18,7 +26,7 @@ public class Fruit : MonoBehaviour
             Vector3 direction = (col.transform.position - transform.position).normalized;
 
             Quaternion rotation = Quaternion.LookRotation(direction);
-            
+
             Debug.Log("WE HIT A WATERMELON");
             Instantiate(fruitSlicedPrefab, transform.position, rotation);
             Destroy(gameObject);
