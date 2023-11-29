263da49dfaa7b2ccf12312449e0b0ca151ede111
y1141335509 <y1141335509@gmail.com>
2023-11-28 23:52:17 -0800

    OMG openai api worked!✌️✌️✌️✌️✌️nb!!!😎

--- kx-profile-backend/app.py
+++ kx-profile-backend/app.py
@@ -3,28 +3,40 @@
 # python3 app.py
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import openai
-import sys
+import os
 
-OPENAI_API_KEY = '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'
 
+# setup OPENAI API KEY:
+# (terminal) export OPENAI_API_KEY='YOUR_OPENAI_API_KEY'
+# next, get API KEY directly from your local environment:
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+print(os.getenv('OPENAI_API_KEY'))
+
+
+conversations = []
+# initialize flask app:
 app = Flask(__name__)
-
-@app.route("/message", methods=['POST'])
+CORS(app=app)       # handle Cross Origin Resource Sharing
+@app.route("/message", methods=['POST'])    # default backend IP is https://127.0.0.1:5000
 def handleMessage():
     data = request.json
     user_message = data['message']
 
     # call Open AI API from here:
-    # response = ...
-    response = openai.completions.create(
-        engine='gpt-3.5-turbo',      # choose the appropriate language model
-        prompt=user_message,
-        max_tokens=300
+    response = openai.chat.completions.create(
+        model='gpt-3.5-turbo',      # choose the appropriate language model
+        messages=[{
+            'role': 'user',
+            'content': user_message,
+        }]
     )
-    reply = response.choices[0].text.strip()
+    reply = response.choices[0].message.content
+    conversations.append(reply)
 
-    return jsonify({'reply': 'Here is AI response ... '})
+    return jsonify({'reply': reply})
 
 
 
