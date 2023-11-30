e84c5d5a7d5a8fd1a8c2cdff852f5155bd1979ea
y1141335509 <y1141335509@gmail.com>
2023-11-30 14:14:20 -0800

    interactive conversation AI tested successfully!😄😊

--- kx-profile-backend/app.py
+++ kx-profile-backend/app.py
@@ -1,54 +1,105 @@
-# LOCAL ENV (run the following in your terminal MAC OS)
-# source ./backend-env/bin/activate
-# python3 app.py
+# # LOCAL ENV (run the following in your terminal MAC OS)
+# # source ./backend-env/bin/activate
+# # python3 app.py
+
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import openai
+# import os
+
+
+# # setup OPENAI API KEY:
+# # (terminal) export OPENAI_API_KEY='YOUR_OPENAI_API_KEY'
+# # next, get API KEY directly from your local environment:
+# openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# print(os.getenv('OPENAI_API_KEY'))
+
+
+
+
+
+# conversations = []
+# # initialize flask app:
+# app = Flask(__name__)
+# CORS(app=app)       # handle Cross Origin Resource Sharing
+# @app.route("/message", methods=['POST'])    # default backend IP is https://127.0.0.1:5000
+# def handleMessage():
+#     data = request.json
+#     user_message = data['message']
+
+#     # call Open AI API from here:
+#     response = openai.chat.completions.create(
+#         model='gpt-3.5-turbo',      # choose the appropriate language model
+#         messages=[{
+#             'role': 'user',
+#             'content': user_message,
+#         }]
+#     )
+#     reply = response.choices[0].message.content
+#     conversations.append(reply)
+
+#     return jsonify({'reply': reply})
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
 import os
 
+# Initialize Flask app
+app = Flask(__name__)
+CORS(app=app)
 
-# setup OPENAI API KEY:
-# (terminal) export OPENAI_API_KEY='YOUR_OPENAI_API_KEY'
-# next, get API KEY directly from your local environment:
+# Load OpenAI API key from environment variable
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-print(os.getenv('OPENAI_API_KEY'))
+# Initialize a dictionary to store conversations by user session
+conversations = {}
 
-
-conversations = []
-# initialize flask app:
-app = Flask(__name__)
-CORS(app=app)       # handle Cross Origin Resource Sharing
-@app.route("/message", methods=['POST'])    # default backend IP is https://127.0.0.1:5000
+@app.route("/message", methods=['POST'])
 def handleMessage():
     data = request.json
     user_message = data['message']
+    session_id = data.get('sessionId', 'default')  # Get or create a session ID for the user
 
-    # call Open AI API from here:
+    # Get the conversation history for this user, or start a new one
+    conversation = conversations.get(session_id, [])
+
+    # Add the user's message to the conversation
+    conversation.append({'role': 'user', 'content': user_message})
+
+
+    # Call OpenAI API including the full conversation history
     response = openai.chat.completions.create(
-        model='gpt-3.5-turbo',      # choose the appropriate language model
-        messages=[{
-            'role': 'user',
-            'content': user_message,
-        }]
+        model='ft:gpt-3.5-turbo-1106:personal::8QiYxFq8',      # Choose the appropriate language model
+        messages=conversation,
     )
-    reply = response.choices[0].message.content
-    conversations.append(reply)
+    ai_reply = response.choices[0].message.content
 
-    return jsonify({'reply': reply})
+    # Add AI's reply to the conversation
+    conversation.append({'role': 'assistant', 'content': ai_reply})
+
+    # Save the updated conversation
+    conversations[session_id] = conversation
 
 
-
-
-
-
+    return jsonify({'reply': ai_reply})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
 
 
 
--- kx-profile/src/App.js
+++ kx-profile/src/App.js
@@ -1,13 +1,66 @@
+// import React, { useState } from 'react';
+// import './App.css';
+
+// function App() {
+//   const [userInput, setUserInput] = useState('');
+//   const [aiResponse, setAiResponse] = useState('');
+
+//   function sendMessageToBackend() {
+//     // post message to backend (127.0.0.1:5000/message)
+//     fetch('http://127.0.0.1:5000/message', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ message: userInput }),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//       console.log('AI回复:', data.reply);
+//       setAiResponse(data.reply); // 更新状态以显示 AI 的回复
+//     })
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <input 
+//           type="text" 
+//           value={userInput} 
+//           onChange={(e) => setUserInput(e.target.value)} 
+//           placeholder="输入您的消息"
+//         />
+//         <button onClick={sendMessageToBackend}>发送</button>
+//         {aiResponse && <p style={{ color: 'white', }}>Katharine的回复: {aiResponse}</p>}
+//         <p>Hello world!!</p>
+//         <br />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [userInput, setUserInput] = useState('');
-  const [aiResponse, setAiResponse] = useState('');
+  const [conversation, setConversation] = useState([]);
 
   function sendMessageToBackend() {
-    // post message to backend (127.0.0.1:5000/message)
+    // 将用户输入添加到对话历史
+    setConversation([...conversation, { role: 'user', content: userInput }]);
+
+    // 发送请求到后端
     fetch('http://127.0.0.1:5000/message', {
       method: 'POST',
       headers: {
@@ -18,20 +71,22 @@ function App() {
     .then(response => response.json())
     .then(data => {
       console.log('AI回复:', data.reply);
-      setAiResponse(data.reply); // 更新状态以显示 AI 的回复
+      // 将 AI 回复添加到对话历史
+      setConversation(prev => [...prev, { role: 'ai', content: data.reply }]);
     })
     .catch((error) => {
       console.error('Error:', error);
+      // 在出错时也更新对话历史
+      setConversation(prev => [...prev, { role: 'ai', content: '发生错误，请重试。' }]);
     });
+
+    // 清空用户输入
+    setUserInput('');
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <input 
           type="text" 
           value={userInput} 
@@ -39,46 +94,17 @@ function App() {
           placeholder="输入您的消息"
         />
         <button onClick={sendMessageToBackend}>发送</button>
-        {aiResponse && <p style={{ color: 'white', }}>AI回复: {aiResponse}</p>}
-        <p>Hello world!!</p>
-        <br />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ textAlign: 'left', color: 'white', marginTop: '10px' }}>
+          {conversation.map((msg, index) => (
+            <p key={index}>
+              {msg.role === 'user' ? '您: ' : 'Katharine的回复: '}
+              {msg.content}
+            </p>
+          ))}
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
