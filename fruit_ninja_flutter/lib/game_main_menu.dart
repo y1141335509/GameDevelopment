67b475eabc2d3b0a6a74ef17bb93e88878c61fe0
y1141335509 <y1141335509@gmail.com>
2023-12-05 13:20:38 -0800

    it looks like a game now🥹

---
+++ fruit_ninja_flutter/lib/game_main_menu.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'canvas_area/canvas_area.dart';
+
+class GameMenuScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Game Menu"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => CanvasArea())),
+              child: Text("Start"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Implement High Score logic here
+                // Example: showHighScores(context);
+              },
+              child: Text("High Score"),
+            ),
+            ElevatedButton(
+              onPressed: () => SystemNavigator.pop(), // This exits the app
+              child: Text("Exit Game"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Example function to show high scores (Implement according to your app logic)
+  void showHighScores(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("High Scores"),
+          content: Text("High scores go here..."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
--- fruit_ninja_flutter/lib/main.dart
+++ fruit_ninja_flutter/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'initial_screen.dart';
+import 'game_main_menu.dart';
 
 void main() {
-  runApp(InitialScreen());
+  // runApp(InitialScreen());
+  runApp(MaterialApp(
+    home: GameMenuScreen(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
