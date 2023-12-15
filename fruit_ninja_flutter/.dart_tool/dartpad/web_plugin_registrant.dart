6d32e05c93a608d5a90e117a41538ae206f5b608
y1141335509 <y1141335509@gmail.com>
2023-12-14 18:45:20 -0800

    fixed ruby bug

--- fruit_ninja_flutter/.dart_tool/dartpad/web_plugin_registrant.dart
+++ fruit_ninja_flutter/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,11 +6,13 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:audioplayers_web/audioplayers_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
