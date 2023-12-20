90c7be5025c252ff069c035a7e70667e6346883d
y1141335509 <y1141335509@gmail.com>
2023-12-19 19:17:39 -0800

    added highest score on UI

--- fruit_ninja_flutter/lib/game_main_menu_components/select_level.dart
+++ fruit_ninja_flutter/lib/game_main_menu_components/select_level.dart
@@ -1,48 +1,68 @@
 import 'package:flutter/material.dart';
 
-import 'levels/level_00.dart';
+import './levels/level_00.dart';
 import './levels/level_01.dart';
 import './levels/level_02.dart';
+import './levels/level_03.dart';
 
 class LevelSelectionScreen extends StatelessWidget {
   const LevelSelectionScreen({Key? key})
-      : super(key: key); // Marked as nullable
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> levels = [
+      CanvasAreaLevel_00(level: 0),
+      CanvasAreaLevel_01(level: 1),
+      CanvasAreaLevel_02(level: 2),
+      CanvasAreaLevel_03(level: 3),
+      // Add more levels as needed
+    ];
+
     return Scaffold(
       appBar: AppBar(title: Text('Select Level')),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('Level 0'),     // THE MOST ORIGINAL LEVEL
+      body: GridView.builder(
+        itemCount: levels.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, // 每行个数
+          mainAxisSpacing: 10.0, // 行间距
+          crossAxisSpacing: 10.0, // 列间距
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return ElevatedCard(
+            level: index,
             onTap: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CanvasAreaLevel_00(level: 0)));
+                context,
+                MaterialPageRoute(builder: (context) => levels[index]),
+              );
             },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ElevatedCard extends StatelessWidget {
+  final int level;
+  final VoidCallback onTap;
+
+  const ElevatedCard({Key? key, required this.level, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      child: InkWell(
+        onTap: onTap,
+        child: Center(
+          child: Text(
+            'Level $level',
+            style: TextStyle(fontSize: 24),
           ),
-          ListTile(
-            title: Text('Level 1'),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CanvasAreaLevel_01(level: 1)));
-            },
-          ),
-          ListTile(
-            title: Text('Level 2'),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => CanvasAreaLevel_02(level: 2)));
-            },
-          ),
-          // Add more levels as needed
-        ],
+        ),
       ),
     );
   }
