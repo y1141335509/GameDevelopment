77d1662abad00e61a1a0d10cf2c73574aed47505
Y1141335509 <Y1141335509@gmail.com>
2024-01-03 17:53:22 -0800

    able to drag map

--- fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++ fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/yinghaiyu/Main/DesktopMain/STUDY/DevProject/GameDevelopment/fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/yinghaiyu/Main/DesktopMain/STUDY/DevProject/GameDevelopment/fruit_ninja_flutter/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
+  /Users/yinghaiyu/Main/DesktopMain/STUDY/GameDevelopment/fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/yinghaiyu/Main/DesktopMain/STUDY/GameDevelopment/fruit_ninja_flutter/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++ fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/yinghaiyu/Main/DesktopMain/STUDY/DevProject/GameDevelopment/fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/yinghaiyu/Main/DesktopMain/STUDY/DevProject/GameDevelopment/fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
+  /Users/yinghaiyu/Main/DesktopMain/STUDY/GameDevelopment/fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/yinghaiyu/Main/DesktopMain/STUDY/GameDevelopment/fruit_ninja_flutter/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
